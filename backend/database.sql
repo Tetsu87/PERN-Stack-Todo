61bece7cee4225ce19347cb492e0c3104a9e2bc7
Tkai87 <tkai@usc.edu>
2022-08-28 15:19:13 -0700

    postgreSQL connected

--- backend/database.sql
+++ backend/database.sql
@@ -1,1 +1,6 @@
-CREAT DATABASE perntodo;
+CREATE DATABASE perntodo;
+
+CREATE TABLE todo(
+    todo_id SERIAL PRIMARY KEY,
+    description VARCHAR(255)
+);
--- backend/server.js
+++ backend/server.js
@@ -1,13 +1,36 @@
 const express = require("express");
 const app = express();
-const cors = require("cors")
+const cors = require("cors");
+const pool = require("./db");
 
-const PORT = 5000
+const PORT = 5000;
 
 // middleware
-app.use(cors())
-app.use(express.json())
+app.use(cors());
+app.use(express.json());
+
+//ROUTES//
+
+//create a todo
+
+app.post("/todos", async (req, res) => {
+    try {
+        const { description } = req.body
+        const newTodo = await pool.query("INSERT INTO todo (description) VALUES($1)", [description])
+        console.log(req.body)
+    } catch (err) {
+        console.log(err)
+    }
+});
+
+//get all todos
+
+//get a todo
+
+//update a todo
+
+//delete a todo
 
 app.listen(PORT, () => {
-    console.log(`server has started on port ${PORT} !!`)
-})
+  console.log(`server has started on port ${PORT} !!`);
+});
