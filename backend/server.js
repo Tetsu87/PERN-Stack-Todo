ccb0df177d3c198a8b4672a42e66dcf6c223db21
Tkai87 <tkai@usc.edu>
2022-08-28 15:56:47 -0700

    added database post/get/put/delete methods

--- backend/server.js
+++ backend/server.js
@@ -14,22 +14,68 @@ app.use(express.json());
 //create a todo
 
 app.post("/todos", async (req, res) => {
-    try {
-        const { description } = req.body
-        const newTodo = await pool.query("INSERT INTO todo (description) VALUES($1)", [description])
-        console.log(req.body)
-    } catch (err) {
-        console.log(err)
-    }
+  try {
+    const { description } = req.body;
+    const newTodo = await pool.query(
+      "INSERT INTO todo (description) VALUES($1) RETURNING *",
+      [description]
+    );
+    // console.log(req.body)
+    res.json(newTodo.rows[0]);
+  } catch (err) {
+    console.log(err);
+  }
 });
 
 //get all todos
+app.get("/todos", async (req, res) => {
+  try {
+    const allTodos = await pool.query("SELECT * FROM todo");
+    res.json(allTodos.rows);
+  } catch (err) {
+    console.log(err);
+  }
+});
 
 //get a todo
+app.get("/todos/:id", async (req, res) => {
+  try {
+    const { id } = req.params;
+    const todo = await pool.query("SELECT * FROM todo WHERE todo_id = $1", [
+      id,
+    ]);
+    res.json(todo.rows[0]);
+    // console.log(req.params);
+  } catch (err) {
+    console.log(err);
+  }
+});
 
 //update a todo
+app.put("/todos/:id", async (req, res) => {
+  try {
+    const { id } = req.params;
+    const { description } = req.body;
+    const updateTodo = await pool.query(
+      "UPDATE todo SET description = $1 WHERE todo_id = $2",
+      [description, id]
+    );
+    res.json("Todo was updated!");
+  } catch (err) {
+    console.log(err);
+  }
+});
 
 //delete a todo
+app.delete("/todos/:id", async (req, res) => {
+    try {
+      const { id } = req.params;
+      const deleteTodo = await pool.query("DELETE FROM todo WHERE todo_id = $1", [id])
+      res.json("Todo was deleted!");
+    } catch (err) {
+      console.log(err);
+    }
+  });
 
 app.listen(PORT, () => {
   console.log(`server has started on port ${PORT} !!`);
